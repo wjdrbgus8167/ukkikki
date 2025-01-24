b0573a2bfee9802e201587de69ef691e96f22868
KzeroJun <rladudwns164@gmail.com>
2025-01-24 16:50:39 +0900

    feat : 여행 계획을 저장하는 entity 생성 - pk는 auto increment로 관리 - PlanningStatus enum 정의

--- ukkikki-be/ukkikki/src/main/java/domain/travelPlan/entity/TravelPlan.java
+++ ukkikki-be/ukkikki/src/main/java/domain/travelPlan/entity/TravelPlan.java
@@ -1,4 +1,36 @@
 package domain.travelPlan.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "travel_plans")
 public class TravelPlan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long travelPlanId;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(nullable = false, name = "start_date")
+    private LocalDateTime startDate;
+
+    @Column(nullable = false, name = "end_date")
+    private LocalDateTime endDate;
+
+    @Column(length = 1000)
+    private String hostComment;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false,name = "planning_status")
+    private PlanningStatus planningStatus;
+
+    @Column(nullable = false, name = "min_people")
+    private int minPeople;
+
+    @Column(nullable = false, name = "max_people")
+    private int maxPeople;
 }
