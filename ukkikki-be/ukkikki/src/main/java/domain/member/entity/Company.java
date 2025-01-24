1c74e923c09e4845346e4416ee2ca1e5aed6135b
KzeroJun <rladudwns164@gmail.com>
2025-01-24 17:36:26 +0900

    feat : 여행사 유저를 저장하는 entity 생성
    - pk는 auto increment로 관리

---
+++ ukkikki-be/ukkikki/src/main/java/domain/member/entity/Company.java
@@ -0,0 +1,41 @@
+package domain.member.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "companies")
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long companyId;
+
+    @Column(nullable = false, length = 30)
+    private String email;
+
+    @Column(nullable = false, length = 64)
+    private String password;
+
+    @Column(nullable = false, name = "ceo_name", length = 20)
+    private String ceoName;
+
+    @Column(nullable = false, name = "company_name", length = 30)
+    private String companyName;
+
+    @Column(nullable = false, name = "business_registration_number", length = 12)
+    private String businessRegistrationNumber;
+
+    @Column(nullable = false, name = "phone_number", length = 11)
+    private String phoneNumber;
+
+    @Column(nullable = false, name = "create_time", updatable = false)
+    private LocalDateTime createTime;
+
+    @Column(nullable = false, name = "delete_time", updatable = false)
+    private LocalDateTime deleteTime;
+
+    @Column(name = "profile_image_url", nullable = false, length = 2000)
+    private String profileImageUrl;
+}
