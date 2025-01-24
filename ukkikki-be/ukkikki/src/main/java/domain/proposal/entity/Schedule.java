7a6d8da28be2ba127b69c7ff61192a3a56fb8e91
KzeroJun <rladudwns164@gmail.com>
2025-01-24 17:29:59 +0900

    feat : 여행 내 일정을 저장하는 entity 생성
    - pk는 auto increment로 관리

---
+++ ukkikki-be/ukkikki/src/main/java/domain/proposal/entity/Schedule.java
@@ -0,0 +1,26 @@
+package domain.proposal.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "schedules")
+public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long scheduleId;
+
+    @Column(nullable = false, name = "schedule_name", length = 255)
+    private String scheduleName;
+
+    @Column(nullable = false, name = "start_time")
+    private LocalDateTime startTime;
+
+    @Column(nullable = false, name = "end_time")
+    private LocalDateTime endTime;
+
+    @Column(name = "image_url", length = 2000)
+    private String imageUrl;
+}
