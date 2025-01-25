16841f7f9f027fdf33d0fb42da7b7b37a07c9529
KzeroJun <rladudwns164@gmail.com>
2025-01-25 19:42:56 +0900

    fix : 제인서(Proposal) 엔티티의 잘못된 연관관계 매핑 수정
    - departureCity와 arrivalCity를 departureAirport와 arrivalAirport로 변경
    - 잘못된 City와의 연관 관계를 Airport로 교체
    - 외래 키 이름은 departure_airport_code와 arrival_airport_code로 수정

--- ukkikki-be/ukkikki/src/main/java/domain/proposal/entity/Proposal.java
+++ ukkikki-be/ukkikki/src/main/java/domain/proposal/entity/Proposal.java
@@ -1,5 +1,6 @@
 package domain.proposal.entity;
 
+import domain.info.Airport;
 import domain.info.City;
 import domain.member.entity.Company;
 import domain.travelPlan.entity.TravelPlan;
@@ -13,74 +14,74 @@ import java.time.LocalDateTime;
 @Table(name = "proposals")
 public class Proposal {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long proposalId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long proposalId;
 
-    @Column(nullable = false, name = "start_date")
-    private LocalDate startDate;
+	@Column(nullable = false, name = "start_date")
+	private LocalDate startDate;
 
-    @Column(nullable = false, name = "end_date")
-    private LocalDate endDate;
+	@Column(nullable = false, name = "end_date")
+	private LocalDate endDate;
 
-    @Column(nullable = false, length = 50)
-    private String airline;
+	@Column(nullable = false, length = 50)
+	private String airline;
 
-    @Column(nullable = false, name = "start_date_boarding_time")
-    private LocalDateTime startDateBoardingTime;
+	@Column(nullable = false, name = "start_date_boarding_time")
+	private LocalDateTime startDateBoardingTime;
 
-    @Column(nullable = false, name = "start_date_arrival_time")
-    private LocalDateTime startDateArrivalTime;
+	@Column(nullable = false, name = "start_date_arrival_time")
+	private LocalDateTime startDateArrivalTime;
 
-    @Column(nullable = false, name = "end_date_boarding_time")
-    private LocalDateTime endDateBoardingTime;
+	@Column(nullable = false, name = "end_date_boarding_time")
+	private LocalDateTime endDateBoardingTime;
 
-    @Column(nullable = false, name = "end_date_arrival_time")
-    private LocalDateTime endDateArrivalTime;
+	@Column(nullable = false, name = "end_date_arrival_time")
+	private LocalDateTime endDateArrivalTime;
 
-    @Column(nullable = false)
-    private int deposit;
+	@Column(nullable = false)
+	private int deposit;
 
-    @Column(nullable = false, name = "min_people")
-    private int minPeople;
+	@Column(nullable = false, name = "min_people")
+	private int minPeople;
 
-    @Column(nullable = false, name = "guide_included")
-    private boolean guideIncluded;
+	@Column(nullable = false, name = "guide_included")
+	private boolean guideIncluded;
 
-    @Column(nullable = false, name = "product_introduction", length = 4000)
-    private String productIntroduction;
+	@Column(nullable = false, name = "product_introduction", length = 4000)
+	private String productIntroduction;
 
-    @Column(nullable = false, name = "refund_policy", length = 4000)
-    private String refundPolicy;
+	@Column(nullable = false, name = "refund_policy", length = 4000)
+	private String refundPolicy;
 
-    @Column(nullable = false, name = "insurance_included")
-    private boolean insuranceIncluded;
+	@Column(nullable = false, name = "insurance_included")
+	private boolean insuranceIncluded;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false, name = "proposal_status")
-    private ProposalStatus proposalStatus;
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, name = "proposal_status")
+	private ProposalStatus proposalStatus;
 
-    @Column(nullable = false, name = "create_time", updatable = false)
-    @CreatedDate
-    private LocalDateTime createTime;
+	@Column(nullable = false, name = "create_time", updatable = false)
+	@CreatedDate
+	private LocalDateTime createTime;
 
-    @Column(name = "update_time")
-    private LocalDateTime updateTime;
+	@Column(name = "update_time")
+	private LocalDateTime updateTime;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "company_id")
-    private Company company;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "company_id")
+	private Company company;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "travel_plan_id")
-    private TravelPlan travelPlan;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "travel_plan_id")
+	private TravelPlan travelPlan;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "departure_city_id")
-    private City departureCity;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "departure_airport_code")
+	private Airport departureAirport;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "arrival_city_id")
-    private City arrivalCity;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "arrival_airport_code")
+	private Airport arrivalAirport;
 
 }
