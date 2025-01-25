d3ca3acc46e423d07b45892e27823d2dda9c58f5
KzeroJun <rladudwns164@gmail.com>
2025-01-25 20:07:31 +0900

    feat : 공항(Airport) 엔티티에 연관관계 매핑 추가
    - City 와 @ManyToOne 관계 추가
    - 각 필드에 외래 키 매핑을 위해 @JoinColumn 설정

--- ukkikki-be/ukkikki/src/main/java/domain/info/Airport.java
+++ ukkikki-be/ukkikki/src/main/java/domain/info/Airport.java
@@ -2,9 +2,12 @@ package domain.info;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -20,4 +23,8 @@ public class Airport {
 
 	@Column(nullable = false, name = "airport_name", length = 20)
 	private String airportName;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "city_id")
+	private City city;
 }
