cd2c60ee41a4e190b546349fcc610f25610220c1
KzeroJun <rladudwns164@gmail.com>
2025-01-25 19:48:10 +0900

    feat : 채팅(Message) 엔티티에 연관관계 매핑 추가
    - UserTravelPlan와 @ManyToOne관계 추가
    - 각 필드에 외래 키 매핑을 위해 @JoinColumn 설정

--- ukkikki-be/ukkikki/src/main/java/domain/travelPlan/entity/Message.java
+++ ukkikki-be/ukkikki/src/main/java/domain/travelPlan/entity/Message.java
@@ -2,9 +2,12 @@ package domain.travelPlan.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -22,4 +25,8 @@ public class Message {
 
 	@Column(nullable = false, name = "created_at")
 	private LocalDate createdAt;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_travel_plan_id")
+	private UserTravelPlan userTravelPlan;
 }
