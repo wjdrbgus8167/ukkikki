00fa1fb9e9a753aeb332e3c21930f08077af93f0
KzeroJun <rladudwns164@gmail.com>
2025-01-25 19:30:49 +0900

    feat : 비용(Cost) 엔티티에 연관관계 매핑 추가
    - Proposal와 @ManyToOne 관계 추가
    - 각 필드에 외래 키 매핑을 위해 @JoinColumn 설정

--- ukkikki-be/ukkikki/src/main/java/domain/proposal/entity/Cost.java
+++ ukkikki-be/ukkikki/src/main/java/domain/proposal/entity/Cost.java
@@ -6,16 +6,20 @@ import jakarta.persistence.*;
 @Table(name = "costs")
 public class Cost {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long costId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long costId;
 
-    @Column(nullable = false, name = "max_people")
-    private int maxPeople;
+	@Column(nullable = false, name = "max_people")
+	private int maxPeople;
 
-    @Column(nullable = false, name = "min_people")
-    private int minPeople;
+	@Column(nullable = false, name = "min_people")
+	private int minPeople;
 
-    @Column(nullable = false)
-    private int cost;
+	@Column(nullable = false)
+	private int cost;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "proposal_id")
+	private Proposal proposal;
 }
