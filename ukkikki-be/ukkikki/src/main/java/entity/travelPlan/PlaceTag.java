71ee126b13f56944168942d64cded708850124c5
김준석 <crush0047@gmail.com>
2025-01-28 01:35:56 +0900

    refactor: entity 패키지 클래스 경로 수정

---
+++ ukkikki-be/ukkikki/src/main/java/entity/travelPlan/PlaceTag.java
@@ -0,0 +1,34 @@
+package entity.travelPlan;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "place_tags")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PlaceTag {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long placeTagId;
+
+	@Column(nullable = false, name = "place_tag_name", length = 50)
+	private String placeTagName;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "place_id")
+	private Place place;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_travel_plan_id")
+	private MemberTravelPlan memberTravelPlan;
+}
---
+++ ukkikki-be/ukkikki/src/main/java/entity/travelPlan/TravelPlan.java
@@ -0,0 +1,49 @@
+package entity.travelPlan;
+
+import entity.info.City;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "travel_plans")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TravelPlan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long travelPlanId;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(nullable = false, name = "start_date")
+    private LocalDate startDate;
+
+    @Column(nullable = false, name = "end_date")
+    private LocalDate endDate;
+
+    @Column(length = 1000)
+    private String hostComment;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false,name = "planning_status")
+    private PlanningStatus planningStatus;
+
+    @Column(nullable = false, name = "min_people")
+    private int minPeople;
+
+    @Column(nullable = false, name = "max_people")
+    private int maxPeople;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "departure_city_id")
+    private City departureCity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "arrival_city_id")
+    private City arrivalCity;
+
+}
