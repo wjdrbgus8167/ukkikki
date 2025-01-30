afe10053e82b35a6ddd6b260e1ca06fe6421cde2
KzeroJun <rladudwns164@gmail.com>
2025-01-30 22:52:35 +0900

    feat : 도시 조회 담당의 책임을 갖는 객체 추가
    - cityId를 통해 RDB에서 조회하는 역할을 수행

---
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/repository/city/CityFinder.java
@@ -0,0 +1,17 @@
+package com.dancing_orangutan.ukkikki.repository.city;
+
+import com.dancing_orangutan.ukkikki.entity.info.City;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CityFinder {
+
+	private final JpaCityRepository jpaCityRepository;
+
+	public City findById(Integer cityId) {
+		return jpaCityRepository.findById(cityId)
+				.orElseThrow(() -> new IllegalArgumentException("해당 도시가 존재하지 않습니다. : " + cityId));
+	}
+}
