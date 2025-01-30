3918c75e3d4efec7b1cb5fe2d24420b086c7d2bf
JH <201667@jnu.ac.kr>
2025-01-31 01:09:30 +0900

    feat: 사용자 인증 객체 커스텀, 관련 서비스 작성
    - 일반 사용자(MemberUserDetails)와 기업 사용자(CompanyUserDetails) 인증 객체 구현
    - CustomUserDetailsService 작성

---
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/global/security/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.dancing_orangutan.ukkikki.global.security;
+
+import com.dancing_orangutan.ukkikki.repository.member.CompanyRepository;
+import com.dancing_orangutan.ukkikki.repository.member.MemberRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@AllArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+    private final CompanyRepository companyRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return memberRepository.findByEmail(email)
+                .map(member -> (UserDetails) new MemberUserDetails(member))
+                .or(() -> companyRepository.findByEmail(email).map(company -> (UserDetails) new CompanyUserDetails(company)))
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + email));
+    }
+
+}
