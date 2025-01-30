d6927b3a9bd0a9826f96eb77343c004b0e5037cd
KzeroJun <rladudwns164@gmail.com>
2025-01-30 22:53:04 +0900

    feat : 도시 데이터 관리 repository 추가
    - Spring Data JPA 사용

---
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/repository/city/JpaCityRepository.java
@@ -0,0 +1,8 @@
+package com.dancing_orangutan.ukkikki.repository.city;
+
+import com.dancing_orangutan.ukkikki.entity.info.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaCityRepository extends JpaRepository<City, Integer> {
+
+}
