6165fb1df546e2569f800619c1fde51336f32399
KzeroJun <rladudwns164@gmail.com>
2025-01-30 22:54:24 +0900

    refactor : TravelPlan 엔티티에 getter 및 빌더 패턴 추가

--- ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/entity/travelPlan/TravelPlan.java
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/entity/travelPlan/TravelPlan.java
@@ -5,10 +5,13 @@ import jakarta.persistence.*;
 
 import java.time.LocalDate;
 import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "travel_plans")
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TravelPlan {
 
@@ -45,5 +48,21 @@ public class TravelPlan {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "arrival_city_id")
     private City arrivalCity;
+
+    @Builder
+    public TravelPlan(Integer travelPlanId, String name, LocalDate startDate, LocalDate endDate,
+            String hostComment, PlanningStatus planningStatus, int minPeople, int maxPeople,
+            City departureCity, City arrivalCity) {
+        this.travelPlanId = travelPlanId;
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.hostComment = hostComment;
+        this.planningStatus = planningStatus;
+        this.minPeople = minPeople;
+        this.maxPeople = maxPeople;
+        this.departureCity = departureCity;
+        this.arrivalCity = arrivalCity;
+    }
 
 }
