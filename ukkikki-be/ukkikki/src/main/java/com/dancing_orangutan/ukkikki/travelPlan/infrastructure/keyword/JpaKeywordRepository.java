deceb1ddcc565756c88091bdde72d8ea3dda345b
KzeroJun <rladudwns164@gmail.com>
2025-01-31 17:19:01 +0900

    feat : 키워드 데이터 관리 repository 추가

---
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/travelPlan/infrastructure/keyword/JpaKeywordRepository.java
@@ -0,0 +1,9 @@
+package com.dancing_orangutan.ukkikki.travelPlan.infrastructure.keyword;
+
+import com.dancing_orangutan.ukkikki.travelPlan.domain.KeywordEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface JpaKeywordRepository extends JpaRepository<KeywordEntity, Integer> {
+}
