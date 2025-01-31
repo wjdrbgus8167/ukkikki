17c26cfccafc0a7fcf22f0ce9645d9bd48d3390a
KzeroJun <rladudwns164@gmail.com>
2025-01-31 17:21:25 +0900

    feat : 키워드 엔티티에 getter 추가

--- ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/travelPlan/domain/KeywordEntity.java
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/travelPlan/domain/KeywordEntity.java
@@ -7,11 +7,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "keywords")
+@Getter
 public class KeywordEntity {
 
 	@Id
