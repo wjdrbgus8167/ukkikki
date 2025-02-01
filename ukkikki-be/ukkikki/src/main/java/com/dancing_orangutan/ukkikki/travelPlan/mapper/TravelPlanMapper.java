c56f6f27d43c1c01ee08d938a371bb78cf660a79
KzeroJun <rladudwns164@gmail.com>
2025-02-01 21:12:44 +0900

    refactor: 매핑 방식 개선

--- ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/travelPlan/mapper/TravelPlanMapper.java
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/travelPlan/mapper/TravelPlanMapper.java
@@ -1,15 +1,23 @@
 package com.dancing_orangutan.ukkikki.travelPlan.mapper;
 
-import com.dancing_orangutan.ukkikki.travelPlan.domain.TravelPlan;
-import com.dancing_orangutan.ukkikki.travelPlan.domain.TravelPlanEntity;
+import com.dancing_orangutan.ukkikki.entity.info.CityEntity;
+import com.dancing_orangutan.ukkikki.travelPlan.domain.travelPlan.TravelPlan;
+import com.dancing_orangutan.ukkikki.travelPlan.domain.travelPlan.TravelPlanEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 //spring container에 Bean으로 등록
 @Mapper(componentModel = "spring")
 public interface TravelPlanMapper {
 
-	TravelPlanEntity domainToEntity(TravelPlan domain);
-
+	@Mapping(source = "arrivalCity", target = "arrivalCityId", qualifiedByName = "cityToId")
+	@Mapping(source = "departureCity", target = "departureCityId", qualifiedByName = "cityToId")
 	TravelPlan entityToDomain(TravelPlanEntity entity);
 
+	@Named("cityToId")
+	static Integer cityToId(CityEntity city) {
+		return city != null ? city.getCityId() : null;
+	}
 }
+
