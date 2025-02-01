677c3618036d2301990e425b4751238e3a36dfd3
KzeroJun <rladudwns164@gmail.com>
2025-02-01 21:07:34 +0900

    feat: 여행 계획 생성 시 필요한 연관 객체 조회 및 설정 추가
    - getReferenceById로 select + insert 문제 해결

--- ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/travelPlan/infrastructure/keyword/KeywordFinder.java
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/travelPlan/infrastructure/keyword/KeywordFinder.java
@@ -1,6 +1,7 @@
 package com.dancing_orangutan.ukkikki.travelPlan.infrastructure.keyword;
 
-import com.dancing_orangutan.ukkikki.travelPlan.domain.KeywordEntity;
+import com.dancing_orangutan.ukkikki.entity.info.CityEntity;
+import com.dancing_orangutan.ukkikki.travelPlan.domain.keyword.KeywordEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -14,4 +15,7 @@ public class KeywordFinder {
         return jpaKeywordRepository.findById(keywordId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 키워드 입니다 : " + keywordId));
     }
 
+    public KeywordEntity getReferenceById(Integer keywordId) {
+        return jpaKeywordRepository.getReferenceById(keywordId);
+    }
 }
