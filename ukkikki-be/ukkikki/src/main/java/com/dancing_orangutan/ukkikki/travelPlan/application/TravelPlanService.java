8109d0ddcfde60b2d1ec3944736f1bd7a07ba980
KzeroJun <rladudwns164@gmail.com>
2025-02-01 21:10:14 +0900

    refactor: 여행 계획 서비스에서 불필요한 의존성 제거 및 리팩터링

--- ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/travelPlan/application/TravelPlanService.java
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/travelPlan/application/TravelPlanService.java
@@ -2,16 +2,11 @@ package com.dancing_orangutan.ukkikki.travelPlan.application;
 
 
 import com.dancing_orangutan.ukkikki.travelPlan.application.command.CreateTravelPlanCommand;
-import com.dancing_orangutan.ukkikki.travelPlan.domain.TravelPlan;
-import com.dancing_orangutan.ukkikki.travelPlan.domain.travelPlanKeyword.TravelPlanKeywordEntity;
-import com.dancing_orangutan.ukkikki.travelPlan.domain.travelPlanKeyword.TravelPlanKeywordId;
-import com.dancing_orangutan.ukkikki.travelPlan.infrastructure.keyword.KeywordFinder;
-import com.dancing_orangutan.ukkikki.travelPlan.infrastructure.travelPlanKeyword.TravelPlanKeywordRegistrant;
+import com.dancing_orangutan.ukkikki.travelPlan.domain.travelPlan.TravelPlan;
 import com.dancing_orangutan.ukkikki.travelPlan.ui.response.CreateTravelPlanResponse;
 import com.dancing_orangutan.ukkikki.travelPlan.ui.response.CreateTravelPlanResponse.TravelPlanResponse;
-import com.dancing_orangutan.ukkikki.entity.info.CityEntity;
-import com.dancing_orangutan.ukkikki.travelPlan.infrastructure.city.CityFinder;
 import com.dancing_orangutan.ukkikki.travelPlan.infrastructure.travelPlan.TravelPlanRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,55 +16,44 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class TravelPlanService {
 
-	private final CityFinder cityFinder;
 	private final TravelPlanRepository travelPlanRepository;
-	private final KeywordFinder keywordFinder;
-	private final TravelPlanKeywordRegistrant travelPlanKeywordRegistrant;
 
 	public CreateTravelPlanResponse createTravelPlan(CreateTravelPlanCommand command) {
 
-		CityEntity departureCity = cityFinder.findById(command.getDepartureCityId());
-		CityEntity arrivalCity = cityFinder.findById(command.getArrivalCityId());
+		List<Integer> keywords = command.getKeywords().stream()
+				.map(CreateTravelPlanCommand.KeywordCommand::getKeywordId)
+				.toList();
 
 		TravelPlan domain = TravelPlan.builder()
+				.departureCityId(command.getDepartureCityId())
+				.arrivalCityId(command.getArrivalCityId())
 				.name(command.getName())
-				.startDate(command.getStartDate())
 				.endDate(command.getEndDate())
+				.startDate(command.getStartDate())
+				.planningStatus(command.getPlanningStatus())
 				.minPeople(command.getMinPeople())
 				.maxPeople(command.getMaxPeople())
-				.departureCity(departureCity)
-				.arrivalCity(arrivalCity)
-				.planningStatus(command.getPlanningStatus())
+				.keywords(keywords)
+				.memberId(command.getMemberId())
+				.adultCount(command.getAdultCount())
+				.infantCount(command.getInfantCount())
+				.childCount(command.getChildCount())
 				.build();
 
 		TravelPlan savedTravelPlan = travelPlanRepository.save(domain);
 
-		command.getKeywords()
-				.stream()
-				.map(keywordCommand -> keywordFinder.findById(keywordCommand.getKeywordId()))
-				.forEach(keywordEntity -> {
-					TravelPlanKeywordEntity travelPlanKeyword = TravelPlanKeywordEntity.builder()
-									.travelPlanKeywordId(TravelPlanKeywordId.builder()
-											.travelPlanId(savedTravelPlan.getTravelPlanId())
-											.keywordId(keywordEntity.getKeywordId()).build())
-											.build();
-					travelPlanKeywordRegistrant.registerTravelPlanKeyword(travelPlanKeyword);
-				});
-
-		TravelPlanResponse response = TravelPlanResponse.builder()
-				.travelPlanId(savedTravelPlan.getTravelPlanId())
-				.name(savedTravelPlan.getName())
-				.arrivalCityId(savedTravelPlan.getArrivalCity().getCityId())
-				.departureCityId(savedTravelPlan.getDepartureCity().getCityId())
-				.maxPeople(savedTravelPlan.getMaxPeople())
-				.minPeople(savedTravelPlan.getMinPeople())
-				.planningStatus(savedTravelPlan.getPlanningStatus())
-				.startDate(savedTravelPlan.getStartDate())
-				.endDate(savedTravelPlan.getEndDate())
-				.build();
-
-		return CreateTravelPlanResponse.builder().
-				travelPlan(response)
-				.build();
+		return CreateTravelPlanResponse.builder()
+				.travelPlan(TravelPlanResponse.builder()
+						.travelPlanId(savedTravelPlan.getTravelPlanId())
+						.name(savedTravelPlan.getName())
+						.departureCityId(savedTravelPlan.getDepartureCityId())
+						.arrivalCityId(savedTravelPlan.getArrivalCityId())
+						.startDate(savedTravelPlan.getStartDate())
+						.endDate(savedTravelPlan.getEndDate())
+						.planningStatus(savedTravelPlan.getPlanningStatus())
+						.minPeople(savedTravelPlan.getMinPeople())
+						.maxPeople(savedTravelPlan.getMaxPeople())
+						.build()
+				).build();
 	}
 }
