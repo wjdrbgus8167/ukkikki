65b1a5a4a7cbe8cc0fe4072b949b4f562707ce58
KzeroJun <rladudwns164@gmail.com>
2025-02-01 21:20:05 +0900

    refactor : 팀 컨벤션에 맞춰 변경

--- ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/repository/member/MemberRepository.java
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/repository/member/MemberRepository.java
@@ -1,12 +1,12 @@
 package com.dancing_orangutan.ukkikki.repository.member;
 
-import com.dancing_orangutan.ukkikki.entity.member.Member;
+import com.dancing_orangutan.ukkikki.entity.member.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Integer> {
-    Optional<Member> findByEmail(String email);
+public interface MemberRepository extends JpaRepository<MemberEntity, Integer> {
+    Optional<MemberEntity> findByEmail(String email);
 }
