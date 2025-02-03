7ef0f187557f503f1b350cf2883bbd20a6435474
김준석 <crush0047@gmail.com>
2025-02-03 14:48:22 +0900

    feat: CreatePlaceTagCommand ì클래스 생성

--- ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/place/application/command/CreatePlaceTagCommand.java
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/place/application/command/CreatePlaceTagCommand.java
@@ -1,4 +1,22 @@
 package com.dancing_orangutan.ukkikki.place.application.command;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class CreatePlaceTagCommand {
+
+    private final Integer placeTagName;
+    private final Integer placeId;
+    private final Integer travelPlanId;
+    private final Integer memberId;
+
+    @Builder
+    public CreatePlaceTagCommand(Integer placeTagName, Integer placeId,
+                                 Integer travelPlanId, Integer memberId) {
+        this.placeTagName = placeTagName;
+        this.placeId = placeId;
+        this.travelPlanId = travelPlanId;
+        this.memberId = memberId;
+    }
 }
