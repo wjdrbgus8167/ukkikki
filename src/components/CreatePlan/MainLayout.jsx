b6cd658a540a54ffbb3083ac92e8ecc090a7dfcd
newww-a <tls001217@naver.com>
2025-02-04 01:32:06 +0900

    update: 사이드바 수정 및 장소 리스트 컴포넌트 추가

--- src/components/CreatePlan/MainLayout.jsx
+++ src/components/CreatePlan/MainLayout.jsx
@@ -2,14 +2,15 @@
 // 사이드바 + 지도 영역
 
 import TravelPlanMap from "./TravelPlanMap";
-import DateSideBar from "./SideBar";
-
+import PlanDateSideBar from "./PlanDateSideBar";
+import TravelPlan from "./TravelPlan";
 
 const MainLayout = () => {
     return (
-        <div>
-            <DateSideBar />
-            <TravelPlanMap />
+        <div className="flex items-start space-x-4">
+            <PlanDateSideBar />
+            <TravelPlan />
+            {/* <TravelPlanMap /> */}
         </div>
     )
 };
--- src/components/CreatePlan/TravelPlan.jsx
+++ src/components/CreatePlan/TravelPlan.jsx
@@ -1,37 +1,20 @@
-import React, { useState } from "react";
-import SaveButton from "./button/SaveButton";
-import AddDayButton from "./button/AddDayButton";
-import DayList from "./button/DayList";
+// 장소 선택 및 등록 하는 컴포넌트 인데 (작성중)
 
+import React from "react";
+import SearchBar from "./SearchBar";
 
 const TravelPlan = () => {
-
-    const [days, setDays] = useState(['1일차','2일차','3일차','4일차']);
-    const [clicked, setClicked] = useState(null);
-
-    
-    const maxDay = 10;
-
-    const addDay = () => {
-        if (days.length < maxDay) {
-            setDays([...days, `${days.length + 1}일차`])
-        }
-    };
-
-    const handleButtonClick = (index) => {
-        setClicked(index);
-    }
-    return (
-        <div className="travel-day flex flex-col space-y-4">
-            <div className="max-h-96 overflow-y-auto">
-                <DayList days={days} onClick={handleButtonClick} clicked={clicked} />
-            </div>
-            
-            <div className="flex flex-col space-y-4 items-start">
-                <AddDayButton onClick={addDay}/>
-                <SaveButton />
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <h1>여행지: ex) 일본</h1>
+      <p>여행 일자: ex) 2025.03.18 ~ 2025.03.24</p>
+      <SearchBar />
+      <button className="border-2 ">우랑이 pick!</button>
+      <ul>
+        <li>목록 리스트1</li>
+        <li>목록 리스트2</li>
+      </ul>
+    </div>
+  );
 };
 export default TravelPlan;
