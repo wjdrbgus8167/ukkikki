31e614f7e55e06526780709763d1b13785cf5b6f
KzeroJun <rladudwns164@gmail.com>
2025-02-05 01:48:11 +0900

    feat : 여행 계획 검색 API 구현

---
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/travelPlan/ui/response/SearchTravelPlanResponse.java
@@ -0,0 +1,24 @@
+package com.dancing_orangutan.ukkikki.travelPlan.ui.response;
+
+import com.dancing_orangutan.ukkikki.travelPlan.constant.PlanningStatus;
+import com.dancing_orangutan.ukkikki.travelPlan.ui.request.KeywordUi;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.LocalDate;
+import java.util.List;
+
+public record SearchTravelPlanResponse(@JsonProperty(namespace = "travelPlans") List<SearchTravelPlanInfo> travelPlanInfoUis) {
+
+	public record SearchTravelPlanInfo(
+			Integer travelPlanId,
+			String name,
+			Integer departureCityId,
+			Integer arrivalCityId,
+			LocalDate startDate,
+			LocalDate endDate,
+			int curPeople,
+			int maxPeople,
+			PlanningStatus planningStatus,
+			List<KeywordUi> keywords
+	) {
+	}
+}
