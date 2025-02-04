c1c91a54063feec743c69c2b680db5594a968f4c
newww-a <tls001217@naver.com>
2025-02-04 17:32:29 +0900

    update: 여행사 디테일 페이지 코드 업데이트

--- src/components/agencyDetail/PlaceList.jsx
+++ src/components/agencyDetail/PlaceList.jsx
@@ -1,29 +1,109 @@
 //장소 리스트
-import React, { useContext } from "react";
-import ProposalDetailContext from "../../contexts/proposalDetailContext";
+
+// import React, { useContext } from "react";
+// import ProposalDetailContext from "../../contexts/proposalDetailContext";
+
+// const PlaceList = () => {
+//   const { proposal } = useContext(ProposalDetailContext);
+
+//   return (
+//     <div className="container">
+//       <div className="travel-place">
+//         {proposal.travelPlan.places.map((place, index) =>(
+//           <div key= {index}>
+//             <h3>{place.name}</h3>
+//             <p>{place.address}</p>
+//               {place.placeTags.map((tag, idx) => (
+//                 <div key ={idx}>
+//                   # {tag.placeTagName}
+//                 </div>
+//               ))}
+//           </div>
+//         ))
+
+//         }
+//       </div>
+
+//     </div>
+//   )
+// }; 
+// export default PlaceList;
+
+
+import React from "react";
 
 const PlaceList = () => {
-  const { proposal } = useContext(ProposalDetailContext);
+  const proposalPlaces = [
+           {
+                name: '글렌 알라키 증류소',
+                address:'Distillery Cottages, Glenallachie, Aberlour AB38 9LR 영국',
+                latitude: 57.47208460679224,
+                longitude: -3.235839587055353,
+                placeTags: [
+                  {
+                    placeTagId: "1",
+                    placeTagName: "가장예쁜 증류소"
+                  },
+                  {
+                    placeTagId: "2",
+                    placeTagName: "전망굿굿"
+                  }
+                ],
+                likesCnt: 10,
+            },
+            {
+                name: '아드벡 증류소',
+                address:'Port Ellen, Isle of Islay PA42 7EA 영국',
+                latitude: 55.640622696525305,
+                longitude: -6.108250915342315,
+                placeTags: [
+                  {
+                    placeTagId: "1",
+                    placeTagName: "피트위스키 중 탑"
+                  },
+                  {
+                    placeTagId: "2",
+                    placeTagName: "맛조음음"
+                  }
+                ],
+                likesCnt: 7,
+            },
+            {
+                name: '라프로익 디스틸러리',
+                address:'Laphroaig, 라프로에그 Isle of Islay PA42 7DU 영국',
+                latitude: 55.63058946287993,
+                longitude: -6.15198947096777,
+                placeTags: [
+                  {
+                    placeTagId: "1",
+                    placeTagName: "피트위스키 중 탑"
+                  }
+                ],
+                likesCnt: 6,
+            },
+  ];
 
   return (
-    <div className="container">
-      <div className="travel-place">
-        {proposal.travelPlan.places.map((place, index) =>(
-          <div key= {index}>
-            <h3>{place.name}</h3>
-            <p>{place.address}</p>
-              {place.placeTags.map((tag, idx) => (
-                <div key ={idx}>
+    <div className="place-container">
+      {proposalPlaces.map((place, idx) => {
+        return (
+          <div key={idx} 
+          className="place border-2 rounded-xl p-2 mb-2 w-auto h-24">
+            <p className="place-name text-m font-semibold">{place.name}</p>
+            <p className="place-address text-xs text-gray-400">{place.address}</p>
+            {place.placeTags.map((tag, index) => {
+              return (
+                <div key={index}
+                className="place-tag text-xs font-semibold" 
+                >
                   # {tag.placeTagName}
-                </div>
-              ))}
+                </div> 
+              );
+              })}
           </div>
-        ))
-
-        }
-      </div>
-
+        );
+      })}
     </div>
-  )
-}; 
+  );
+};
 export default PlaceList;
--- src/components/agencyDetail/ProposalContent.jsx
+++ src/components/agencyDetail/ProposalContent.jsx
@@ -1,38 +1,61 @@
 //제안서 디테일(계획)
 
-import React, { useContext } from "react";
-import ProposalDetailContext from "../../contexts/proposalDetailContext";
+// import React, { useContext } from "react";
+// import ProposalDetailContext from "../../contexts/proposalDetailContext";
+
+// const ProposalContent = () => {
+//   const { proposal, totalCount } = useContext(ProposalDetailContext);
+
+//   if(!proposal) {
+//     return <div>제안서 불러오는 중,,</div>
+//   }
+
+//   return (
+//     <div className="container">
+//       <div className="travel-plan">
+//         <h2 className="travel-title"> 여행타이틀: {proposal.data.travelPlan.name}</h2>
+//         <p>출발: {proposal.travelPlan.departureCityName}</p>
+//         <p>도착: {proposal.travelPlan.arrivalCityName}</p>
+//         <p>여행 예상 날짜: {proposal.travelPlan.startDate} ~ {proposal.data.travelPlan.endDate}</p>
+//         <p>테마:{proposal.travelPlan.keyword.join(",")}</p>
+//         <p>총 참여인원: {totalCount}</p>
+//         <p>내용: </p>
+//       </div>
+
+//       <div className="message">
+//         <h3>메세지</h3>
+//         <ul>
+//           {proposal.messages.map((message, index) => (
+//             <li key={index}>
+//               <p>{message.content}</p>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// };
+// export default ProposalContent;
+
+import React from "react";
 
 const ProposalContent = () => {
-  const { proposal, totalCount } = useContext(ProposalDetailContext);
-
-  if(!proposal) {
-    return <div>제안서 불러오는 중,,</div>
-  }
-
   return (
-    <div className="container">
+    <div>
       <div className="travel-plan">
-        <h2 className="travel-title"> 여행타이틀: {proposal.data.travelPlan.name}</h2>
-        <p>출발: {proposal.travelPlan.departureCityName}</p>
-        <p>도착: {proposal.travelPlan.arrivalCityName}</p>
-        <p>여행 예상 날짜: {proposal.travelPlan.startDate} ~ {proposal.data.travelPlan.endDate}</p>
-        <p>테마:{proposal.travelPlan.keyword.join(",")}</p>
-        <p>총 참여인원: {totalCount}</p>
+      <span className="flex w-[475px] h-[163px] justify-center items-start font-['Pretendard'] text-[96px] font-extrabold leading-[114.563px] text-[#000] absolute top-[55px] left-0 text-center whitespace-nowrap z-[89]">
+            술친자들의
+            <br />
+            증류소 여행
+          </span>
+        <p>출발: 서울(인천)</p>
+        <p>도착: 영국(스코틀랜드)</p>
+        <p>여행 예상 날짜: 2025.03.10 ~ 2025.03.</p>
+        <p>테마:</p>
+        <p>총 참여인원: </p>
         <p>내용: </p>
       </div>
-
-      <div className="message">
-        <h3>메세지</h3>
-        <ul>
-          {proposal.messages.map((message, index) => (
-            <li key={index}>
-              <p>{message.content}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
     </div>
-  );
+  )
 };
 export default ProposalContent;
