430cd82fe926c2110fc5b8f6b0675aefb1cfc71c
KzeroJun <rladudwns164@gmail.com>
2025-02-05 21:07:11 +0900

    feat : 여행 계획 마감 일시 API

--- ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/travelPlan/ui/TravelPlanController.java
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/travelPlan/ui/TravelPlanController.java
@@ -4,12 +4,14 @@ import com.dancing_orangutan.ukkikki.global.security.MemberUserDetails;
 import com.dancing_orangutan.ukkikki.global.util.ApiUtils;
 import com.dancing_orangutan.ukkikki.travelPlan.application.command.CreateTravelPlanCommand;
 import com.dancing_orangutan.ukkikki.travelPlan.application.command.JoinTravelPlanCommand;
+import com.dancing_orangutan.ukkikki.travelPlan.application.command.UpdateCloseTimeCommand;
 import com.dancing_orangutan.ukkikki.travelPlan.application.command.WriteCommentCommand;
 import com.dancing_orangutan.ukkikki.travelPlan.application.query.FetchSuggestedTravelPlanQuery;
 import com.dancing_orangutan.ukkikki.travelPlan.application.query.SearchTravelPlanQuery;
 import com.dancing_orangutan.ukkikki.travelPlan.constant.PlanningStatus;
 import com.dancing_orangutan.ukkikki.travelPlan.ui.request.CreateTravelPlanRequest;
 import com.dancing_orangutan.ukkikki.travelPlan.ui.request.JoinTravelPlanRequest;
+import com.dancing_orangutan.ukkikki.travelPlan.ui.request.UpdateCloseTimeRequest;
 import com.dancing_orangutan.ukkikki.travelPlan.ui.request.WriteCommentRequest;
 import com.dancing_orangutan.ukkikki.travelPlan.ui.response.CreateTravelPlanResponse;
 import com.dancing_orangutan.ukkikki.travelPlan.application.TravelPlanService;
@@ -34,7 +36,8 @@ public class TravelPlanController {
 
 	@PostMapping
 	public ApiUtils.ApiResponse<CreateTravelPlanResponse> createTravelPlan(
-			@RequestBody CreateTravelPlanRequest request, @AuthenticationPrincipal MemberUserDetails memberUserDetails) {
+			@RequestBody CreateTravelPlanRequest request,
+			@AuthenticationPrincipal MemberUserDetails memberUserDetails) {
 		CreateTravelPlanCommand command = request.toCommand(memberUserDetails.getMemberId());
 
 		return ApiUtils.success(travelPlanService.createTravelPlan(command));
@@ -54,12 +57,12 @@ public class TravelPlanController {
 
 	@GetMapping("/search")
 	public ApiUtils.ApiResponse<SearchTravelPlanResponse> searchTravelPlan(
-			@RequestParam(value = "startDate")LocalDate startDate,
+			@RequestParam(value = "startDate") LocalDate startDate,
 			@RequestParam(value = "endDate") LocalDate endDate,
 			@RequestParam(value = "departureCityId") Integer departureCityId,
 			@RequestParam(value = "arrivalCityId") Integer arrivalCityId,
-			@RequestParam(value ="keyword",required = false) List<Integer> keywords,
-			@RequestParam(value = "status",required = false)PlanningStatus status){
+			@RequestParam(value = "keyword", required = false) List<Integer> keywords,
+			@RequestParam(value = "status", required = false) PlanningStatus status) {
 
 		SearchTravelPlanQuery searchQuery = SearchTravelPlanQuery
 				.builder()
@@ -78,13 +81,15 @@ public class TravelPlanController {
 
 	//여행사 제안받은 여행 계획서 조회
 	@GetMapping("/list")
-	public ApiUtils.ApiResponse<FetchSuggestedTravelPlanResponse> fetchSuggestedTravelPlans(){
-		FetchSuggestedTravelPlanQuery query = new FetchSuggestedTravelPlanQuery(PlanningStatus.BIDDING);
+	public ApiUtils.ApiResponse<FetchSuggestedTravelPlanResponse> fetchSuggestedTravelPlans() {
+		FetchSuggestedTravelPlanQuery query = new FetchSuggestedTravelPlanQuery(
+				PlanningStatus.BIDDING);
 		return ApiUtils.success(travelPlanService.fetchSuggestedTravelPlans(query));
 	}
 
 	@PutMapping("/{travelPlanId}/comments")
-	public ApiUtils.ApiResponse<String> writeComments(@RequestBody WriteCommentRequest request, @PathVariable(name = "travelPlanId") Integer travelPlanId) {
+	public ApiUtils.ApiResponse<String> writeComments(@RequestBody WriteCommentRequest request,
+			@PathVariable(name = "travelPlanId") Integer travelPlanId) {
 		WriteCommentCommand command = WriteCommentCommand.builder()
 				.travelPlanId(travelPlanId)
 				.hostComment(request.hostComment())
@@ -94,5 +99,19 @@ public class TravelPlanController {
 		travelPlanService.writeComment(command);
 
 		return ApiUtils.success("코멘트가 성공적으로 등록되었습니다.");
+	}
+
+	@PutMapping("/{travelPlanId}/closeTime")
+	public ApiUtils.ApiResponse<String> updateCloseTime(@RequestBody UpdateCloseTimeRequest request,
+			@PathVariable(name = "travelPlanId") Integer travelPlanId) {
+
+		UpdateCloseTimeCommand command = UpdateCloseTimeCommand.builder()
+				.closeTime(request.closeTime())
+				.travelPlanId(travelPlanId)
+				.build();
+
+		command.validate();
+		travelPlanService.updateCloseTime(command);
+		return ApiUtils.success("마감일시가 성공적으로 등록되었습니다.");
 	}
 }
