25e92e98a9e20e743e883776c5f670c46a79475c
wjdrbgus8167 <wjdrbgus8167@naver.com>
2025-02-05 16:49:47 +0900

    fix: utils import 수정

--- ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/proposal/ui/ProposalController.java
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/proposal/ui/ProposalController.java
@@ -1,8 +1,9 @@
 package com.dancing_orangutan.ukkikki.proposal.ui;
 
 
-import com.dancing_orangutan.ukkikki.global.response.ApiUtils;
+
 import com.dancing_orangutan.ukkikki.global.security.CompanyUserDetails;
+import com.dancing_orangutan.ukkikki.global.util.ApiUtils;
 import com.dancing_orangutan.ukkikki.proposal.application.ProposalService;
 import com.dancing_orangutan.ukkikki.proposal.application.command.CreateProposalCommand;
 import com.dancing_orangutan.ukkikki.proposal.domain.proposal.Proposal;
