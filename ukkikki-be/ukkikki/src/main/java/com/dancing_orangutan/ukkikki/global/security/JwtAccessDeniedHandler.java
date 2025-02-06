0b5aa24cbcd763c888e6daa352a33a2cd3542791
박주현 <ashlely0739@naver.com>
2025-02-06 11:02:39 +0900

    feat: 인증 필터 예외 Error Code 적용

--- ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/global/security/JwtAccessDeniedHandler.java
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/global/security/JwtAccessDeniedHandler.java
@@ -1,11 +1,10 @@
 package com.dancing_orangutan.ukkikki.global.security;
 
-import com.dancing_orangutan.ukkikki.global.util.ApiUtils;
+import com.dancing_orangutan.ukkikki.global.error.ErrorCode;
 import com.dancing_orangutan.ukkikki.global.util.JsonResponseUtils;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
@@ -15,7 +14,6 @@ public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        ApiUtils.ApiResponse<?> errorResponse = ApiUtils.error("ACCESS_DENIED", "접근 권한이 없습니다.", HttpStatus.FORBIDDEN);
-        JsonResponseUtils.sendJsonResponse(response, HttpStatus.FORBIDDEN, errorResponse);
+        JsonResponseUtils.sendJsonErrorResponse(response, ErrorCode.ACCESS_DENIED);
     }
 }
