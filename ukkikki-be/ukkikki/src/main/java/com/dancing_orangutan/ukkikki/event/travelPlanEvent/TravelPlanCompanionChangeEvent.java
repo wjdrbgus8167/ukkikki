5a00ca9064ce6f1b02a2a67a4f003e455f5357ee
KzeroJun <rladudwns164@gmail.com>
2025-02-06 10:29:33 +0900

    chore : Spring Events 초기 세팅

---
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/event/travelPlanEvent/TravelPlanCompanionChangeEvent.java
@@ -0,0 +1,6 @@
+package com.dancing_orangutan.ukkikki.event.travelPlanEvent;
+
+import com.dancing_orangutan.ukkikki.event.common.Event;
+
+public record TravelPlanCompanionChangeEvent() implements Event {
+}
