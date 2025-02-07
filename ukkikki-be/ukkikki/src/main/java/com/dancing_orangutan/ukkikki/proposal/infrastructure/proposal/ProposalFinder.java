3c9093bb6f314a00c0ce556a4e096c397aff017c
wjdrbgus8167 <wjdrbgus8167@naver.com>
2025-02-07 17:20:07 +0900

    feat : 제안서 조회 추가

--- ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/proposal/infrastructure/proposal/ProposalFinder.java
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/proposal/infrastructure/proposal/ProposalFinder.java
@@ -1,6 +1,7 @@
 package com.dancing_orangutan.ukkikki.proposal.infrastructure.proposal;
 
 import com.dancing_orangutan.ukkikki.proposal.domain.proposal.Proposal;
+import com.dancing_orangutan.ukkikki.proposal.domain.proposal.ProposalEntity;
 import com.dancing_orangutan.ukkikki.proposal.mapper.ProposalMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -10,9 +11,14 @@ import org.springframework.stereotype.Component;
 public class ProposalFinder {
 
     private final ProposalRepository proposalRepository;
+    private final JpaProposalRepository jpaProposalRepository;
 
     public Proposal getProposalDomain(Integer proposalId) {
 
         return proposalRepository.findById(proposalId);
+    }
+
+    public ProposalEntity findByProposalId(Integer proposalId) {
+        return jpaProposalRepository.findByProposalId(proposalId);
     }
 }
