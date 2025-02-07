144e741accb33d716e230d4645592a29d9952af0
박주현 <ashlely0739@naver.com>
2025-02-07 09:57:43 +0900

    feat: FE 도메인 수정

--- ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/global/config/CorsConfig.java
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/global/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://fe.i12c204.p.ssafy.io:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);  // 쿠키 전송 허용
