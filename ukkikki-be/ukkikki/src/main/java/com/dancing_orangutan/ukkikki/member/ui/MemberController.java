7808e70b2722a73cf33e467e3994211dbb079b90
박주현 <ashlely0739@naver.com>
2025-02-07 17:04:23 +0900

    feat: 프로필 조회 기능

---
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/member/ui/MemberController.java
@@ -0,0 +1,35 @@
+package com.dancing_orangutan.ukkikki.member.ui;
+
+import com.dancing_orangutan.ukkikki.global.security.MemberUserDetails;
+import com.dancing_orangutan.ukkikki.global.util.ApiUtils;
+import com.dancing_orangutan.ukkikki.member.application.MemberService;
+import com.dancing_orangutan.ukkikki.member.ui.response.GetMemberProfileResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/profile")
+    public ResponseEntity<ApiUtils.ApiResponse<?>> getMemberProfile(
+            @AuthenticationPrincipal MemberUserDetails memberUserDetails
+    ){
+        return ResponseEntity.ok(
+                ApiUtils.success(
+                        GetMemberProfileResponse.builder()
+                                .email(memberUserDetails.getEmail())
+                                .name(memberUserDetails.getName())
+                                .profileImageUrl(memberUserDetails.getProfileImageUrl())
+                                .build()
+                )
+        );
+    }
+
+}
