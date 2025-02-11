e29bbd6e46c27f352e8af0d9c8ad644257880de4
wjdrbgus8167 <wjdrbgus8167@naver.com>
2025-02-11 14:58:26 +0900

    refactor : 에러 핸들링 추가

--- ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/proposal/infrastructure/airport/AirportFinder.java
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/proposal/infrastructure/airport/AirportFinder.java
@@ -2,6 +2,7 @@ package com.dancing_orangutan.ukkikki.proposal.infrastructure.airport;
 
 import com.dancing_orangutan.ukkikki.geography.domain.AirportEntity;
 import com.dancing_orangutan.ukkikki.geography.infrastructure.JpaAirportRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,7 @@ public class AirportFinder {
     private final JpaAirportRepository jpaAirportRepository;
 
     public AirportEntity getReferenceById(String airportCode) {
-        return jpaAirportRepository.getReferenceById(airportCode);
+        return jpaAirportRepository.findById(airportCode)
+                .orElseThrow(() -> new EntityNotFoundException("해당 공항을 찾을 수 없습니다."));
     }
 }
--- ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/proposal/infrastructure/travelPlan/TravelPlanFinder.java
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/proposal/infrastructure/travelPlan/TravelPlanFinder.java
@@ -2,6 +2,7 @@ package com.dancing_orangutan.ukkikki.proposal.infrastructure.travelPlan;
 
 import com.dancing_orangutan.ukkikki.travelPlan.domain.travelPlan.TravelPlanEntity;
 import com.dancing_orangutan.ukkikki.travelPlan.infrastructure.travelPlan.JpaTravelPlanRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,7 @@ public class TravelPlanFinder {
     private final JpaTravelPlanRepository jpaTravelPlanRepository;
 
     public TravelPlanEntity getReferenceById(Integer travelPlanId) {
-        return jpaTravelPlanRepository.getReferenceById(travelPlanId);
+        return jpaTravelPlanRepository.findById(travelPlanId)
+                .orElseThrow(() -> new EntityNotFoundException("해당 여행 걔획방을 찾을 수 없습니다."));
     }
 }
