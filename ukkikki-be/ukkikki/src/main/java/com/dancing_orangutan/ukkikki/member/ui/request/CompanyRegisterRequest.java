06681781ed56641909a8b0017f3e4dd449fa5b27
박주현 <ashlely0739@naver.com>
2025-02-11 18:03:11 +0900

    feat: auth에 @Valid 적용

--- ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/member/ui/request/CompanyRegisterRequest.java
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/member/ui/request/CompanyRegisterRequest.java
@@ -1,14 +1,22 @@
 package com.dancing_orangutan.ukkikki.member.ui.request;
 
+import com.dancing_orangutan.ukkikki.member.validator.*;
 import lombok.Getter;
 
 @Getter
 public class CompanyRegisterRequest {
+    @ValidEmail
     private String email;
+    @ValidPassword
     private String password;
+    @ValidMemberName
     private String ceoName;
+    @ValidCompanyName
     private String companyName;
-    private String businessRegistrationNumber;
+    @ValidBusinessRegistrationNumber
+    private String businessRegistrationNumber; // 숫자 10자리
+    @ValidPhoneNumber
     private String phoneNumber;
+    @ValidImageUrl
     private String profileImageUrl;
 }
