b5b1ec09fa1ee569258e8067443ebc5965e9bf3b
KzeroJun <rladudwns164@gmail.com>
2025-02-11 13:29:04 +0900

    feat : 여행 계획 관련 채팅 메시지 조회 기능
    - 현재 날짜 기준 전 채팅 기록 조회

--- ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/chat/domain/MessageRepository.java
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/chat/domain/MessageRepository.java
@@ -1,7 +1,12 @@
 package com.dancing_orangutan.ukkikki.chat.domain;
 
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface MessageRepository {
 
     MessageEntity save(MessageEntity message);
+
+    List<MessageEntity> findAllByTravelPlanIdAndCreatedAtBefore(Integer travelPlanId, LocalDateTime createdAtBefore);
+
 }
