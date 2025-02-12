6ff3dfd6d5794c4d8a0a5ef95ae1193e08334134
wjdrbgus8167 <wjdrbgus8167@naver.com>
2025-02-13 08:57:20 +0900

    feat : 답변 조회 기능 추가

--- ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/proposal/infrastructure/inquiry/JpaInquiryRepository.java
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/proposal/infrastructure/inquiry/JpaInquiryRepository.java
@@ -4,8 +4,11 @@ import com.dancing_orangutan.ukkikki.proposal.domain.Inquiry.InquiryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface JpaInquiryRepository extends JpaRepository<InquiryEntity, Integer> {
 
     List<InquiryEntity> findByProposal_ProposalId(Integer proposalId);
+
+    Optional<InquiryEntity> findByInquiryIdAndProposal_ProposalId(Integer inquiryId, Integer proposalId);
 }
