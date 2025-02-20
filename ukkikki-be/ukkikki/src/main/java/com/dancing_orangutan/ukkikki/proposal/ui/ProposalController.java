617b3cbbf8fcd11821c192895ff699327ad0f09b
김준석 <crush0047@gmail.com>
2025-02-20 11:36:38 +0900

    fix: isHost의 메소드 명 변경

--- ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/proposal/ui/ProposalController.java
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/proposal/ui/ProposalController.java
@@ -269,7 +269,7 @@ public class ProposalController {
 
         String token = proposalService.generateToken(
                 proposalId,
-                getConnectionRequest.isHost(),
+                getConnectionRequest.getIsHost(),
                 getConnectionRequest.getMemberName()
         );
 
--- ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/proposal/ui/request/GetConnectionRequest.java
+++ ukkikki-be/ukkikki/src/main/java/com/dancing_orangutan/ukkikki/proposal/ui/request/GetConnectionRequest.java
@@ -11,4 +11,10 @@ public class GetConnectionRequest {
     public void setMemberName(String memberName) {
         this.memberName = memberName;
     }
+    public void setIsHost(boolean isHost) {
+        this.isHost = isHost;
+    }
+    public boolean getIsHost() {
+        return isHost;
+    }
 }
